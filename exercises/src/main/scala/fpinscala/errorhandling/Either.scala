d52246242c7231cc39e29423f4f95b9a6fb6a1c6
Takayuki Matsubara <takayuki.1229@gmail.com>
2018-06-16 21:38:12 +0900

    Chapter 4

--- exercises/src/main/scala/fpinscala/errorhandling/Either.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Either.scala
@@ -4,21 +4,54 @@ package fpinscala.errorhandling
 import scala.{Option => _, Either => _, Left => _, Right => _, _} // hide std library `Option` and `Either`, since we are writing our own in this chapter
 
 sealed trait Either[+E,+A] {
- def map[B](f: A => B): Either[E, B] = ???
+ def map[B](f: A => B): Either[E, B] = {
+   this match {
+     case Left(x) => Left(x)
+     case Right(x) => Right(f(x))
+   }
+ }
 
- def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = ???
+ def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = {
+   this match {
+     case Left(x) => Left(x)
+     case Right(x) => f(x)
+   }
+ }
 
- def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = ???
+ def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = {
+   this match {
+     case Left(_) => b
+     case Right(x) => Right(x)
+   }
+ }
 
- def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = ???
+ def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = {
+   this.flatMap(argA => b map(argB => f(argA, argB)))
+ }
+
+ // map2 with `for`
+ def map2ByFor[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = {
+   for {
+     argA <- this
+     argB <- b
+   } yield f(argA, argB)
+ }
 }
 case class Left[+E](get: E) extends Either[E,Nothing]
 case class Right[+A](get: A) extends Either[Nothing,A]
 
 object Either {
-  def traverse[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] = ???
+  def traverse[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] = {
+    // es.foldRight(Right(Nil))((x, acc) => f(x).map2(acc)((xx, yy) => xx :: yy))
+    // [error]  found   : fpinscala.errorhandling.Either[E,List[B]]
+    // [error]  required: fpinscala.errorhandling.Right[scala.collection.immutable.Nil.type]
+    // [error]     es.foldRight(Right(Nil))((x, acc) => f(x).map2(acc)((xx, yy) => xx :: yy))
+    es.foldRight[Either[E,List[B]]](Right(Nil))((x, acc) => f(x).map2(acc)((xx, yy) => xx :: yy))
+  }
 
-  def sequence[E,A](es: List[Either[E,A]]): Either[E,List[A]] = ???
+  def sequence[E,A](es: List[Either[E,A]]): Either[E,List[A]] = {
+    traverse(es)((x) => x)
+  }
 
   def mean(xs: IndexedSeq[Double]): Either[String, Double] = 
     if (xs.isEmpty) 
--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -4,15 +4,34 @@ package fpinscala.errorhandling
 import scala.{Option => _, Some => _, Either => _, _} // hide std library `Option`, `Some` and `Either`, since we are writing our own in this chapter
 
 sealed trait Option[+A] {
-  def map[B](f: A => B): Option[B] = ???
+  def map[B](f: A => B): Option[B] = {
+    this match {
+      case None => None
+      case Some(x) => Some(f(x))
+    }
+  }
 
-  def getOrElse[B>:A](default: => B): B = ???
+  def getOrElse[B>:A](default: => B): B = {
+    this match {
+      case None => default
+      case Some(x) => x
+    }
+  }
 
-  def flatMap[B](f: A => Option[B]): Option[B] = ???
+  def flatMap[B](f: A => Option[B]): Option[B] = {
+    map(f) getOrElse None
+  }
 
-  def orElse[B>:A](ob: => Option[B]): Option[B] = ???
+  def orElse[B>:A](ob: => Option[B]): Option[B] = {
+    map(Some(_)) getOrElse ob
+  }
 
-  def filter(f: A => Boolean): Option[A] = ???
+  def filter(f: A => Boolean): Option[A] = {
+    this match {
+      case Some(x) if (f(x)) => this
+      case _ => None
+    }
+  }
 }
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
@@ -38,11 +57,27 @@ object Option {
   def mean(xs: Seq[Double]): Option[Double] =
     if (xs.isEmpty) None
     else Some(xs.sum / xs.length)
-  def variance(xs: Seq[Double]): Option[Double] = ???
+  def variance(xs: Seq[Double]): Option[Double] = {
+    mean(xs) flatMap(m => mean(xs.map(x => math.pow(x - m, 2))))
+  }
 
-  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = ???
+  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = {
+    a flatMap(argA => b map(argB => f(argA, argB)))
+    // (a, b) match {
+    //   case (None, _) => None
+    //   case (_, None) => None
+    //   case (a, b) => f(a, b)
+    // }
+  }
 
-  def sequence[A](a: List[Option[A]]): Option[List[A]] = ???
+  def sequence[A](a: List[Option[A]]): Option[List[A]] = {
+    // [error]  found   : fpinscala.errorhandling.Option[List[A]]
+    // [error]  required: fpinscala.errorhandling.Some[scala.collection.immutable.Nil.type]
+    // a.foldRight(Some(Nil))((x, acc) => map2(x, acc)((xx, yy) => xx :: yy))
+    a.foldRight[Option[List[A]]](Some(Nil))((x, acc) => map2(x, acc)((xx, yy) => xx :: yy))
+  }
 
-  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = ???
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = {
+    a.foldRight[Option[List[B]]](Some(Nil))((x, acc) => map2(f(x), acc)((xx, yy) => xx :: yy))
+  }
 }
